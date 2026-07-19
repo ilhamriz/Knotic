1be9cc25c56c0a50e242cc92eabc5b584ab74c4c
ilhamriz <milhamrizky7@gmail.com>
2026-07-19 17:27:36 +0700

    update domain name

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,5 +1,5 @@
 export const siteUrl =
-  process.env.NEXT_PUBLIC_SITE_URL ?? "https://knotic.vercel.app";
+  process.env.NEXT_PUBLIC_SITE_URL ?? "https://use-knotic.vercel.app";
 
 export function buildMetadata(title: string, description: string) {
   return {
